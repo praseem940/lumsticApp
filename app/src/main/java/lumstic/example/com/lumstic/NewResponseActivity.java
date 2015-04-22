6cc39a86d7244b87804b6c453dfe064d35f1e933
work <praseem@userworkstech.com>
2015-04-22 18:20:00 +0530

    dynamic layout for cb and rb

--- app/src/main/java/lumstic/example/com/lumstic/NewResponseActivity.java
+++ app/src/main/java/lumstic/example/com/lumstic/NewResponseActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,11 +13,15 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -32,24 +37,27 @@ public class NewResponseActivity extends Activity {
 
     TextView questionNumber;
     TextView questionText;
+    RadioButton button;
+    RadioGroup rg;
+    TableRow row;
 
 
        RelativeLayout fieldContainer;
        LayoutInflater inflater;
-//
-//    TextView answerText;
-//    RelativeLayout imageContainer;
-//    ImageView imageView;
-//    Uri picUri;
-//    int CAMERA_REQUEST = 1;
-//    final int PIC_CROP = 2;
-//    ArrayList<Integer> layouts;
-//    Spinner spinner;
+
+    TextView answerText;
+    RelativeLayout imageContainer;
+    ImageView imageView;
+    Uri picUri;
+    int CAMERA_REQUEST = 1;
+    final int PIC_CROP = 2;
+    Spinner spinner;
 
 
     List<Questions> questionsList;
     List<String> questions;
     List<Integer> layouts;
+    Questions currentQuestions;
 
 
    // int layouts[] = {R.layout.answer_single_line, R.layout.answer_multi_line, R.layout.answer_date_picker, R.layout.answer_image_picker, R.layout.answer_numeric, R.layout.answer_rating, R.layout.answer_multi_choice, R.layout.answer_dropdown,R.layout.answer_dropdown};
@@ -82,6 +90,7 @@ public class NewResponseActivity extends Activity {
         previousQuestion = (Button) findViewById(R.id.previous_question);
 
         questionText.setText(questions.get(0));
+        currentQuestions=questionsList.get(0);
         fieldContainer.removeAllViews();
         inflater = getLayoutInflater();
        fieldContainer.addView(inflater.inflate(layouts.get(0), null));
@@ -95,121 +104,161 @@ public class NewResponseActivity extends Activity {
         nextQuestion.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 questionCounter++;
+                currentQuestions=questionsList.get(questionCounter);
                 fieldContainer.removeAllViews();
                 fieldContainer.addView(inflater.inflate(layouts.get(questionCounter),null));
               questionText.setText(questions.get(questionCounter));
-
+                CheckQuestion(layouts.get(questionCounter),currentQuestions);
             }
         });
         previousQuestion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 questionCounter--;
+                currentQuestions=questionsList.get(questionCounter);
                 fieldContainer.removeAllViews();
                fieldContainer.addView(inflater.inflate(layouts.get(questionCounter), null));
                 questionText.setText(questions.get(questionCounter));
+                CheckQuestion(layouts.get(questionCounter),currentQuestions);
+
 
             }
         });
     }
 
-//    public void CheckQuestion(int layoutId) {
-//        //for date questions
-//        if (layoutId == R.layout.answer_date_picker) {
-//            answerText = (TextView) findViewById(R.id.answer_text);
-//            answerText.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//                    Calendar c = Calendar.getInstance();
-//                    int mYear = c.get(Calendar.YEAR);
-//                    int mMonth = c.get(Calendar.MONTH);
-//                    int mDay = c.get(Calendar.DAY_OF_MONTH);
-//                    System.out.println("the selected " + mDay);
-//                    DatePickerDialog dialog = new DatePickerDialog(NewResponseActivity.this,
-//                            new mDateSetListener(), mYear, mMonth, mDay);
-//                    dialog.show();
-//                }
-//            });
-//        }
-//        //for image questions
-//        if (layoutId == R.layout.answer_image_picker) {
-//            LinearLayout linearLayout = (LinearLayout) findViewById(R.id.answer_text);
-//            RelativeLayout deleteImageRelativeLayout = (RelativeLayout) findViewById(R.id.image_container);
-//            imageView = (ImageView) findViewById(R.id.image);
-//            imageContainer = (RelativeLayout) findViewById(R.id.image_container);
-//            linearLayout.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
+    public void CheckQuestion(int layoutId,Questions currentQuestions) {
+        //for date questions
+        if (layoutId == R.layout.answer_date_picker) {
+           TextView answerText = (TextView) findViewById(R.id.answer_text);
+            answerText.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Calendar c = Calendar.getInstance();
+                    int mYear = c.get(Calendar.YEAR);
+                    int mMonth = c.get(Calendar.MONTH);
+                    int mDay = c.get(Calendar.DAY_OF_MONTH);
+                    System.out.println("the selected " + mDay);
+                    DatePickerDialog dialog = new DatePickerDialog(NewResponseActivity.this,
+                            new mDateSetListener(), mYear, mMonth, mDay);
+                    dialog.show();
+                }
+            });
+        }
+        //for image questions
+        if (layoutId == R.layout.answer_image_picker) {
+            LinearLayout linearLayout = (LinearLayout) findViewById(R.id.answer_text);
+            RelativeLayout deleteImageRelativeLayout = (RelativeLayout) findViewById(R.id.image_container);
+           ImageView imageView = (ImageView) findViewById(R.id.image);
+           imageContainer = (RelativeLayout) findViewById(R.id.image_container);
+            linearLayout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                }
+            });
+
+            deleteImageRelativeLayout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    imageContainer.setVisibility(View.GONE);
+                }
+            });
+
+        }
+
+        if(layoutId==R.layout.answer_dropdown){
+           Spinner spinner =(Spinner)findViewById(R.id.drop_down);
+            List<String> listOptions = new ArrayList<String>();
+            for(int i=0;i<currentQuestions.getOptions().size();i++)
+            {
+                listOptions.add(currentQuestions.getOptions().get(i).getContent());
+            }
+            ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_spinner_dropdown_item, listOptions);
+            spinner.setAdapter(dataAdapter);
+        }
+
+
+
+        if(currentQuestions.getType().contains("RadioQuestion")){
+            rg = new RadioGroup(this);
+            rg.setOrientation(RadioGroup.VERTICAL);
+            for (int i = 0; i < currentQuestions.getOptions().size(); i++) {
+
+                button = new RadioButton(this);
+                rg.addView(button);
+                button.setText(currentQuestions.getOptions().get(i).getContent());
+                button.setId(i);
+                button.setButtonDrawable(R.drawable.custom_radio_button);
+
+            }
+
+                fieldContainer.addView(rg);
+        }
+
+        if(currentQuestions.getType().contains("MultiChoiceQuestion")){
+            LinearLayout ll = new LinearLayout(this);
+            ll.setOrientation(LinearLayout.VERTICAL);
+            for (int i = 0; i < currentQuestions.getOptions().size(); i++)
+            {
+
+                CheckBox checkBox = new CheckBox(this);
+                //checkBox.setOnCheckedChangeListener(this);
+                checkBox.setId(i);
+                checkBox.setText(currentQuestions.getOptions().get(i).getContent());
+                checkBox.setTextSize(20);
+                checkBox.setButtonDrawable(R.drawable.custom_checkbox);
+                ll.addView(checkBox);
+            }
+            fieldContainer.addView(ll);
+        }
+
+
+    }
+
+    //for camera returned picture
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == CAMERA_REQUEST) {
+                Bundle extras = data.getExtras();
+                Bitmap thePic = extras.getParcelable("data");
+                imageView.setImageBitmap(thePic);
+
+//                picUri = data.getData();
+//                performCrop(picUri);
+            }
+
+        }
 //
-//                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-//                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
-//                }
-//            });
-//
-//            deleteImageRelativeLayout.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//                    imageContainer.setVisibility(View.GONE);
-//                }
-//            });
-//
-//        }
-//
-//        if(layoutId==R.layout.answer_dropdown){
-//            spinner =(Spinner)findViewById(R.id.drop_down);
-//            List<String> list = new ArrayList<String>();
-//            list.add("item 1");
-//            list.add("item 2");
-//            list.add("item 3");
-//            ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
-//                    android.R.layout.simple_spinner_dropdown_item, list);
-//            spinner.setAdapter(dataAdapter);
-//        }
-//
-//
-//    }
-//
-//    //for camera returned picture
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//
-//        if (resultCode == RESULT_OK) {
-//            if (requestCode == CAMERA_REQUEST) {
-//                Bundle extras = data.getExtras();
-//                Bitmap thePic = extras.getParcelable("data");
-//                imageView.setImageBitmap(thePic);
-//
-////                picUri = data.getData();
-////                performCrop(picUri);
-//            }
-//
-//        }
-////
-//    }
-//
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.new_response, menu);
-//        return true;
-//    }
-//
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        int id = item.getItemId();
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//
-//    }
-//
-//    class mDateSetListener implements DatePickerDialog.OnDateSetListener {
-//
-//        public void onDateSet(DatePicker view, int year, int monthOfYear,
-//                              int dayOfMonth) {
-//            int mYear = year;
-//            int mMonth = monthOfYear;
-//            int mDay = dayOfMonth;
-//            answerText.setText(new StringBuilder().append(mMonth + 1).append("/").append(mDay).append("/").append(mYear).append(" ").toString());
-//        }
-//    }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.new_response, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    class mDateSetListener implements DatePickerDialog.OnDateSetListener {
+
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            int mYear = year;
+            int mMonth = monthOfYear;
+            int mDay = dayOfMonth;
+            answerText.setText(new StringBuilder().append(mMonth + 1).append("/").append(mDay).append("/").append(mYear).append(" ").toString());
+        }
+    }
     public void checkQuestionType(String questionType, int position)
     {
         if (questionType.contains("PhotoQuestion"))
